03a2d4f73d83e24e1ceadaceef20567ef64bde9e
Patrick O'Neill <patrick.oneill46@gmail.com>
2016-04-03 23:10:29 +0100

    Profile page

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -3,5 +3,7 @@
 angular.module('teamApp')
   .controller('ProfileCtrl', function ($scope) {
     $scope.name = 'Profile';
-    console.log($scope.name);
+    $scope.username = 'Patrick';
+    $scope.primaryPosition = 'Back row';
+    $scope.positions = ['Loosehead', 'Hooker', 'Tighthead', 'Second row', 'Back row', 'Scrum half', 'Out half', 'Centre', 'Wing', 'Fullback'];
   });
