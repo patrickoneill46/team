558465f8d693af0e07365395fa5c082d2e1869b8
Patrick O'Neill <patrick.oneill46@gmail.com>
2016-04-03 21:46:10 +0100

    Adding angular and bootstrap

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name belsizeApp
+ * @description
+ * # belsizeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('teamApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+
+  });
